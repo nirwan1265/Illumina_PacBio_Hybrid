5eaf9487ee838ff7f8513480a21256de167ae7e7
nirwan1265 <tandukar.nirwan@gmail.com>
2023-04-06 14:38:46 -0400

    update

--- src/simulate_pacbio.sh
+++ src/simulate_pacbio.sh
@@ -1,1 +1,16 @@
-https://github.com/yukiteruono/pbsim3
+# Clone the git repository
+# > git clone https://github.com/yukiteruono/pbsim3.git
+# > ./configure
+# > make
+# > make install
+# > pbsim -h
+
+pbsim --strategy wgs --method errhmm --errhmm data/ERRHMM-RSII.model --depth 20 --genome sample/sample.fasta
+
+# strategy = wgs
+# method = errhmm (error model)
+# genome = fasta file
+# depth = coverage (max 20.0)
+# length-min = ( min 100)
+# length-max = (max 1000000)
+# --accuracy-min = ( min 0.75)
