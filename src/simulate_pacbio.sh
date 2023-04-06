0df62a760eca9fe853c7dcb516fee7f7aa15cf03
nirwan1265 <tandukar.nirwan@gmail.com>
2023-04-06 15:31:15 -0400

    update

--- src/simulate_pacbio.sh
+++ src/simulate_pacbio.sh
@@ -5,8 +5,6 @@
 # > make install
 # > pbsim -h
 
-<<<<<<< HEAD
-=======
 pbsim --strategy wgs --method errhmm --errhmm data/ERRHMM-RSII.model --depth 20 --genome sample/sample.fasta
 
 # strategy = wgs
@@ -16,4 +14,4 @@ pbsim --strategy wgs --method errhmm --errhmm data/ERRHMM-RSII.model --depth 20 
 # length-min = ( min 100)
 # length-max = (max 1000000)
 # --accuracy-min = ( min 0.75)
->>>>>>> 5eaf9487ee838ff7f8513480a21256de167ae7e7
+
