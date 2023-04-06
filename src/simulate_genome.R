c941971fba0e676b2d780398376d4a049bc7ccfd
nirwan1265 <tandukar.nirwan@gmail.com>
2023-04-06 09:57:34 -0400

    update

--- src/simulate_genome.R
+++ src/simulate_genome.R
@@ -1,4 +1,3 @@
-
 # Simulate a genome
 set.seed(123)
 
@@ -10,18 +9,23 @@ drepeat <- paste0(rep(c("ATCG", "CGTA", "GTAC", "TGCA"), each = 1), collapse = "
 longrepeat <- paste0(rep(c("GGGGGGGGGG", "AAAAAAAAAAAAAAAA","CCCCCCCCCCC","TTTTTTTTTTT"), each = 1), collapse = "")
 
 # Define frequencies of each repeat type
-monorepeat_freq <- 0.2
-direpeat_freq <- 0.2
-trirepeat_freq <- 0.1
-drepeat_freq <- 0.1
-longrepeat_freq <- 0.05
+monorepeat_freq <- 0.01
+direpeat_freq <- 0.01
+trirepeat_freq <- 0.01
+drepeat_freq <- 0.01
+longrepeat_freq <- 0.01
 nonrepeat_freq <- 1 - monorepeat_freq - direpeat_freq - trirepeat_freq - drepeat_freq - longrepeat_freq
 
+# Simulating the genome based on the frequencies
 repeats <- c(monorepeats, direpeats, trirepeats, drepeat, longrepeat)
 repeat_freqs <- c(monorepeat_freq, direpeat_freq, trirepeat_freq, drepeat_freq, longrepeat_freq)
-seq <- sample(repeats, 100000, replace = TRUE, prob = repeat_freqs)
+seq <- sample(repeats, 100, replace = TRUE, prob = repeat_freqs)
 seq <- paste(seq, collapse = "")
 seq <- paste(sample(c(rep("N", 60)), 1), seq, sample(c(rep("N", 60)), 1), sep = "")
 
+# Sequence name and description
+seqname <- "Simulated1"
 
+# Saving a fasta file
+write.fasta(sequences = seq, names = seqname, file.out = "simulated1.fa")
 
---
+++ src/simulate_illumina.py
@@ -0,0 +1,5 @@
+import rnftools
+https://rnftools.readthedocs.io/en/latest/tutorial/02_simulation.html
+
+
+https://github.com/yukiteruono/pbsim3
