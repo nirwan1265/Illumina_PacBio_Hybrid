9c0b588c832f540e7ea7dcb4ca4211a8c7ee258f
nirwan1265 <tandukar.nirwan@gmail.com>
2023-04-17 10:12:57 -0400

    update

--- src/assembly_spades.sh
+++ src/assembly_spades.sh
@@ -26,5 +26,5 @@ spades.py --careful -t 4 \
 -o "${output_dir}/${filename}"
 done
 
-./spades.py --careful -t 16 -1 /home/ntanduk/CS_simulated_data/illumina/illumina_simulated_l50_c30_simulated_genome_500k_AT_1500_50_pe1.fq -2 /home/ntanduk/CS_simulated_data/illumina/illumina_simulated_l50_c30_simulated_genome_500k_AT_1500_50_pe2.fq --pacbio /home/ntanduk/CS_simulated_data/pacbio/simulated_pacbio_c20_l12000_simulated_genome_500k_AT_1500_50__0001.fastq --trusted-contigs /home/ntanduk/CS_simulated_data/reference/simulated_genome_500k_AT_1500_50.fa -o results/Illumina_AT_1500_50_pacbio_assembly
+./spades.py --isolate -t 16 -1 /home/ntanduk/CS_simulated_data/illumina/illumina_simulated_l50_c30_simulated_genome_500k_AT_1500_50_pe1.fq -2 /home/ntanduk/CS_simulated_data/illumina/illumina_simulated_l50_c30_simulated_genome_500k_AT_1500_50_pe2.fq --pacbio /home/ntanduk/CS_simulated_data/pacbio/simulated_pacbio_c20_l12000_simulated_genome_500k_AT_1500_50__0001.fastq --trusted-contigs /home/ntanduk/CS_simulated_data/reference/simulated_genome_500k_AT_1500_50.fa -o results/Illumina_AT_1500_50_pacbio_assembly
 
